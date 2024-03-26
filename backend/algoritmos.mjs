cfbc48a48c49579118c1e7d5d7b2b28abcdc93bb
Joao Victor <joaovictorxl120@gmail.com>
2024-03-26 19:41:59 -0300

    quicksort recursivo para ordenacao

---
+++ backend/algoritmos.mjs
@@ -0,0 +1,14 @@
+export function quickSort(array) {
+    if(array.length < 2) return array // se for um número apenas, retorne-o
+    else {
+        const pivot = array[0];
+        let maiores = [];
+        let menores = [];
+        for(let i=1;i<array.length;i++){
+            array[i] >= pivot ? maiores.push(array[i]) : menores.push(array[i])
+        }
+        return quickSort(menores).concat([pivot], quickSort(maiores)) // Olha a recursividade aí meu povo...
+    }
+}
+// const array = [3,10,8,6]
+// console.log(quickSort(array))
