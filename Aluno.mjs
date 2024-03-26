ff9f11c6aabb9b806440d0c2979d43e54b4ae4fc
Joao Victor <joaovictorxl120@gmail.com>
2024-03-25 22:49:12 -0300

    pagina e classe

---
+++ Aluno.mjs
@@ -0,0 +1,10 @@
+export class Aluno {
+    constructor(nome, ra, idade, sexo, media, resultado) {
+        this.nome = nome;
+        this.ra = ra;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.media = media;
+        this.resultado = resultado;
+    }
+}
---
+++ script.js
@@ -0,0 +1,2 @@
+import { Aluno } from "./Aluno.mjs";
+
