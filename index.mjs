3e9db0a56a6485121b4a63918e00446e9b4fe112
Joao Victor <joaovictorxl120@gmail.com>
2024-04-01 17:48:56 -0300

    bug do botao voltar consertado

--- index.mjs
+++ index.mjs
@@ -16,27 +16,31 @@ const alunos = [];
 const input = document.querySelector('.opcao');
 const ok = document.querySelector('.ok');
 
-ok.addEventListener('click', () => {
-    switch(input.value){
-        case '1':
-            window.location.href = 'cadastro.html';
-            break
-        case '2':
-            tipoRelatorio = 2
-            window.location.href = 'relatorio.html';
-            break
-        case '3':
-            alert('3');
-            tipoRelatorio = 3
-            break;
-        case '4':
-            alert('4');
-            tipoRelatorio = 4
-            break;
-        case '5':
-            alert('5');
-            break
-        default:
-            alert('Insira uma opção válida.')
-    };
-})
+document.addEventListener('DOMContentLoaded', () => { // Para evitar problemas com as páginas seguintes, devo esperar o DOM carregar o conteúdo antes de executar o script.
+    const ok = document.querySelector('.ok');
+
+    ok.addEventListener('click', () => {
+        switch(input.value){
+            case '1':
+                window.location.href = 'cadastro.html';
+                break
+            case '2':
+                tipoRelatorio = 2
+                window.location.href = 'relatorio.html';
+                break
+            case '3':
+                alert('3');
+                tipoRelatorio = 3
+                break;
+            case '4':
+                alert('4');
+                tipoRelatorio = 4
+                break;
+            case '5':
+                alert('5');
+                break
+            default:
+                alert('Insira uma opção válida.')
+        };
+    });
+});
